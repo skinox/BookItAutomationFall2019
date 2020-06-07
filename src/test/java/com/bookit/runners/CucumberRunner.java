e75d7ad8889b5d9775a03219cdb855c0809bda1a
skinox <jumaev@Aidars-MacBook-Pro.local>
2020-06-07 13:39:08 -0400

    bookit commit

--- src/test/java/com/bookit/runners/CucumberRunner.java
+++ src/test/java/com/bookit/runners/CucumberRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         plugin = {
                 "json:target/cucumber.json"
         },
-        tags = "@ui",
+//        tags = "@ui",
         dryRun = false
 )
 public class CucumberRunner {
